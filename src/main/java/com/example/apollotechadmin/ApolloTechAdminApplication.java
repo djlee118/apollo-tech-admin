e27257ef844b7241acc52dd167f7711e627e95ff
djlee118 <djlee118@nate.com>
2022-02-11 15:25:09 +0900

    Initial commit

---
+++ src/main/java/com/example/apollotechadmin/ApolloTechAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.apollotechadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApolloTechAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloTechAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apollotechadmin/ApolloTechAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apollotechadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloTechAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
